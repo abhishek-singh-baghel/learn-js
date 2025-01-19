c39a42a9e02586b08715c8ba81202affbcd23e4c
Abhishek Singh Baghel <134964964+abhishek-singh-baghel@users.noreply.github.com>
2025-01-19 16:08:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let age = 30
+let name = "abhishek"
+let isAdult = true
+let height = 5.7
+let count
+let isNull = null
+let isDefined = undefined
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof isAdult)
+console.log(typeof count)
+console.log(typeof isNull)
+console.log(typeof isDefined)
+console.log(typeof height)
